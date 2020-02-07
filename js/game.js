e901353ab2bff58de491129fa621116a8781d05d
Rocco <rocco@pohdor.net>
2020-02-07 14:30:01 +0100

    Add back button, rename files

--- js/game.js
+++ js/game.js
@@ -103,7 +103,7 @@ define(
 				var updateInfobox = function(id, props) {
 					var htmlInner = '<div style="width: 400px; position: fixed; left: 70px; color: black;">';
 					var score_ = gameData.getScore();
-					htmlInner += '<h2>Finde die Stadt!<span id="score" style="float: right"></span></h2>'
+					htmlInner += '<button><a href="map.html">Zurück</a></button><h2>Finde die Stadt!<span id="score" style="float: right"></span></h2>'
 					htmlInner += '<button id="checkLocationButton" style="font-size : 20px; width: 100%; height: 100%;;margin:auto;display:block">Prüfe Position</button>'
 					var imageUrl = gameData.getImageUrl();
 					var imageGeoPosition = gameData.getImageGeoPosition();
